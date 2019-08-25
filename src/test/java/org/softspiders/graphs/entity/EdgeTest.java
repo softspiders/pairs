ce635cc43ba36a312e92bc8d785602a923306bd9
AlexanderLapygin <AlexanderLapygin@gmail.com>
2019-08-25 23:09:39 +0300

    test: creation

--- src/test/java/org/softspiders/graphs/entity/EdgeTest.java
+++ src/test/java/org/softspiders/graphs/entity/EdgeTest.java
@@ -9,22 +9,32 @@ import static org.junit.Assert.*;
 public class EdgeTest {
 
 	@Test
-	public void createByUUIDsOfTwoNodes() {
+	public void createByIdsOfTwoNodes() {
 		new Edge(UUID.randomUUID(), UUID.randomUUID());
 	}
 
+	@Test
+	public void creationByIdShouldSetFirstNodeId() {
+		assertNotNull(new Edge(UUID.randomUUID(), UUID.randomUUID()).nodeId1);
+	}
+
+	@Test
+	public void creationByIdShouldSetSecondNodeId() {
+		assertNotNull(new Edge(UUID.randomUUID(), UUID.randomUUID()).nodeId2);
+	}
+
 	@Test(expected = NullPointerException.class /* no exception expected */)
-	public void creationWithFirstUUIDNullIdShouldThrowNPE() {
+	public void creationWithFirstIdNullIdShouldThrowNPE() {
 		new Edge(null, UUID.randomUUID());
 	}
 
 	@Test(expected = NullPointerException.class /* no exception expected */)
-	public void creationWithSecondUUIDNullIdShouldThrowNPE() {
+	public void creationWithSecondIdNullIdShouldThrowNPE() {
 		new Edge(UUID.randomUUID(), null);
 	}
 
 	@Test(expected = NullPointerException.class /* no exception expected */)
-	public void creationWithBothUUIDNullIdShouldThrowNPE() {
+	public void creationWithBothIdsNullIdShouldThrowNPE() {
 		new Edge(null, null);
 	}
 }
