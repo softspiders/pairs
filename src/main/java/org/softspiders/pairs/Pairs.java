40418e3a0a81d7154819175a1fc4678c2a007262
AlexanderLapygin <AlexanderLapygin@gmail.com>
2019-08-29 07:47:59 +0300

    test: first tests for Pairs

--- src/main/java/org/softspiders/pairs/Pairs.java
+++ src/main/java/org/softspiders/pairs/Pairs.java
@@ -1,10 +1,11 @@
 package org.softspiders.pairs;
 
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 
 public class Pairs {
-	public Set<Pair> pairs = new HashSet<Pair>();
+	private Set<Pair> pairs = new HashSet<Pair>();
 
 	public void addPair(Pair pair) {
 		pairs.add(pair);
@@ -12,5 +13,13 @@ public class Pairs {
 
 	public void removePair(Pair pair) {
 		pairs.remove(pair);
+	}
+
+	public Set<Pair> getPairs() {
+		return Collections.unmodifiableSet(pairs);
+	}
+
+	public void setPairs(Set<Pair> pairs) {
+		this.pairs = pairs;
 	}
 }
--- src/test/java/org/softspiders/pairs/PairsTest.java
+++ src/test/java/org/softspiders/pairs/PairsTest.java
@@ -3,7 +3,12 @@ package org.softspiders.pairs;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.HashSet;
+import java.util.Set;
 import java.util.UUID;
+
+import static org.junit.Assert.assertNotSame;
+import static org.junit.Assert.fail;
 
 public class PairsTest {
 	UUID pid = UUID.randomUUID();
@@ -11,11 +16,49 @@ public class PairsTest {
 	UUID sid2 = UUID.randomUUID();
 
 	@Test
+	public void justAfterCreationThePairsShouldBeEmpty() {
+		Assert.assertEquals(0, new Pairs().getPairs().size());
+	}
+
+	@Test
+	public void pairCollectionShouldBeSettable() {
+		Pairs pairs = new Pairs();
+		Set<Pair> pairSet = new HashSet<Pair>() {{ add(new Pair(pid, sid1, sid2));}};
+		pairs.setPairs(pairSet);
+		Assert.assertEquals(pairSet, pairs.getPairs());
+	}
+
+	@Test
+	public void getPairsShouldNotBeExactlyTheSameThatWasInstalled() {
+		Pairs pairs = new Pairs();
+		Set<Pair> pairSet = new HashSet<Pair>() {{ add(new Pair(pid, sid1, sid2));}};
+		pairs.setPairs(pairSet);
+		assertNotSame(pairSet, pairs.getPairs());
+	}
+
+	@Test
 	public void addPair() {
 		Pairs pairs = new Pairs();
 		Pair pair = new Pair(pid, sid1, sid2);
 		pairs.addPair(pair);
-		Assert.assertTrue(pairs.pairs.contains(pair));
+		Assert.assertTrue(pairs.getPairs().contains(pair));
+	}
+
+	@Test
+	public void repeatingAddSamePairShouldBeSilent() {
+		Pairs pairs = new Pairs();
+		Pair pair = new Pair(pid, sid1, sid2);
+		pairs.addPair(pair);
+		pairs.addPair(pair);
+	}
+
+	@Test
+	public void pairsShouldBeSet() {
+		Pairs pairs = new Pairs();
+		Pair pair = new Pair(pid, sid1, sid2);
+		pairs.addPair(pair);
+		pairs.addPair(pair);
+		Assert.assertEquals(1, pairs.getPairs().size());
 	}
 
 	@Test
@@ -24,7 +67,13 @@ public class PairsTest {
 		Pair pair = new Pair(pid, sid1, sid2);
 		pairs.addPair(pair);
 		pairs.removePair(pair);
-		Assert.assertFalse(pairs.pairs.contains(pair));
+		Assert.assertFalse(pairs.getPairs().contains(pair));
+	}
+
+	@Test
+	public void removeNonExistedPairShouldBeSilent() {
+		Pairs pairs = new Pairs();
+		pairs.removePair(new Pair(pid, sid1, sid2));
 	}
 
 }
